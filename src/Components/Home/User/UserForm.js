f1f2facae46eec12911fb31d7bf3b0fa10fc8eeb
Raktim02 <raktimproloy@gmail.com>
2021-06-19 23:57:30 +0600

    shafi

--- src/Components/Home/User/UserForm.js
+++ src/Components/Home/User/UserForm.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './user.css'
 const UserForm = () => {
     return (
-        <div style={{width:'50%'}} className='text-center'>
+        <div style={{width:'50%', margin:'2% auto'}} className='text-center'>
             <form action="">
                 <div className='d-flex justiy-content-around mb-4'>
-                    <label htmlFor="userEmail"  className='me-5 text-light'><h5>email</h5></label>
+                    <label htmlFor="userEmail"  className='me-5 text-light interface-name'><h5>Email</h5></label>
                     <input type="email" id='userEmail' className='form-control' placeholder='Enter Your Email'/>
                 </div>
                 <div className='d-flex justiy-content-around mb-4' >
-                    <label htmlFor="Role"  className='me-5  text-light'><h5>Role</h5></label>
+                    <label htmlFor="Role"  className='me-5  text-light interface-name'><h5>Role</h5></label>
                     <select name="role" id="role" className='form-control'>
                         <option value="admin">Admin</option>
                         <option value="oparetors">Oparetors</option>
@@ -17,14 +17,14 @@ const UserForm = () => {
                     </select>
                 </div>
                 <div className='d-flex justiy-content-around mb-4'>
-                    <label htmlFor="name" className='me-5 text-light'><h5>Name</h5></label>
+                    <label htmlFor="name" className='me-5 text-light interface-name'><h5>Name</h5></label>
                     <input type="text" id='name' placeholder='Enter Your Name' className='form-control'/>
                 </div>
                 <div className='d-flex justiy-content-around mb-4'>
-                    <label htmlFor="position"  className='me-5 text-light'><h5>Position</h5></label>
+                    <label htmlFor="position"  className='me-5 text-light interface-name'><h5>Position</h5></label>
                     <input type="text" id='position' placeholder='Enter Your position' className='form-control'/>
                 </div>
-                <button className="btn btn-primary">Create/Save</button>
+                <button className="btn btn-primary create-button">Create/Save</button>
             </form>
         </div>
     );
